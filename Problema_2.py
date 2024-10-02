b934be1a680ed041887d75c7d9eb65509b6f6353
AkameKM <167798143+AkameKM@users.noreply.github.com>
2024-10-02 02:00:15 +0000

    Problema_2

---
+++ Problema_2.py
@@ -0,0 +1,19 @@
+############################# PROBLEMA 1 ############################
+
+def lista_calificaciones():
+    while True:
+        try:
+            lista = input("Ingrese la lista de calificaciones separadas por comas: ")
+            
+            calificaciones = lista.split(',')
+            
+            calificaciones_con_comas = [int(calificacion.strip()) for calificacion in calificaciones]
+            
+            return calificaciones_con_comas
+        
+        except ValueError:
+        
+            print("ERROR: TODAS LAS CALIFICACIONES DEBEN SER NÚMEROS ENTEROS Y DEBEN ESTAR SEPARADOS POR COMAS")
+
+calificaciones = lista_calificaciones()
+print("Lista de calificaciones ingresadas:", calificaciones)
