cde0f60f0610b8e06fdba0bb2defddea67b5acb6
AkameKM <167798143+AkameKM@users.noreply.github.com>
2024-10-02 02:24:18 +0000

    Problema_3

--- Problema_3.py
+++ Problema_3.py
@@ -1,1 +1,18 @@
-############################# PROBLEMA 2 ############################
+############################# PROBLEMA 3 ############################
+import math  
+
+class CIRCULO:
+    def __init__(self, radio):
+        self.radio = radio
+    
+    def calcular_area(self):
+        return math.pi * self.radio ** 2  
+
+radio_1= float(input("Ingrese el radio del primer círculo: "))
+circulo_1 = CIRCULO(radio_1)  
+
+radio_2= float(input("Ingrese el radio del segundo círculo: "))
+circulo_2 = CIRCULO(radio_2)  
+
+print(f"El área del primer círculo con radio {circulo_1.radio} es: {circulo_1.calcular_area():.2f}")
+print(f"El área del segundo círculo con radio {circulo_2.radio} es: {circulo_2.calcular_area():.2f}")
